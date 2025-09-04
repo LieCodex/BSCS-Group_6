8f4eb86c1aabbf5534241893e9856096edefea84
leekirito <143766872+LieCodex@users.noreply.github.com>
2025-09-04 13:56:21 +0000

    Add update.sh script

---
+++ app/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /var/www/RizGroup/app
+git reset --hard
+git pull origin main
+composer install --optimize-autoloader --no-dev
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
